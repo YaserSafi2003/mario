37b022106c188aa8696c9d6ff16296dfc4367b15
YaserSafi2003 <108479085+YaserSafi2003@users.noreply.github.com>
2022-10-27 20:12:30 +0200

    Add files via upload

---
+++ play.js
@@ -0,0 +1,22 @@
+
+for (let mario=0; mario<100; mario++){
+
+    let mario = 4;
+    let hisFathers = 5 || 2;
+    console.log("mario > 4 || hisFather >4")
+    if (1) {
+     console.log("Your son is in a compartment with a smaller number");}
+     else console.log (" Your son is in a compartment with a larger number")
+     if (2) {
+       console.log("Your son is in a compartment with a smaller number");}
+       else console.log (" Your son is in a compartment with a larger number")
+       if (3) {
+         console.log("Your son is in a compartment with a smaller number");}
+         else console.log (" Your son is in a compartment with a larger number")
+         if (4) {
+           console.log("Your son is in a compartment with a smaller number");}
+           else console.log (" Your son is in a compartment with a larger number")
+           if (5) {
+             console.log("you find your son");}
+             else console.log (" you can not find your son")
+}
